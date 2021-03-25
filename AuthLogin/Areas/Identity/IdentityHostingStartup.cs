9064e7c5848c89f6467bee5fd9dac83cf8903aed
abdelghafor lachkar <lachkar.dev@hotmail.com>
2021-03-25 14:35:31 +0100

    ASP.NET Core 3.1 API for User Management, Authentication and Registration

---
+++ AuthLogin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AuthLogin.Areas.Identity.Data;
+using AuthLogin.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthLogin.Areas.Identity.IdentityHostingStartup))]
+namespace AuthLogin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthLoginContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthLoginContextConnection")));
+
+                services.AddDefaultIdentity<AuthLoginUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<AuthLoginContext>();
+            });
+        }
+    }
+}
